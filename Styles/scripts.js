b43af9034cbf9823a7b0cae0a7347e74296446a8
Besnir <125321585+Besnir@users.noreply.github.com>
2025-03-24 22:07:09 +0300

    Add files via upload

--- Styles/scripts.js
+++ Styles/scripts.js
@@ -7,3 +7,19 @@ window.addEventListener("load", () => {
     document.body.removeChild("loader");
   })
 })
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  const slides = document.querySelector(".slides");
+  slideIndex++;
+  slides.style.transform = `translateX(-${(slideIndex % slides.children.length) * 100}%)`;
+  setTimeout(showSlides, 2000);
+}
+
+function plusSlides(n) {
+  const slides = document.querySelector(".slides");
+  slideIndex += n;
+  slides.style.transform = `translateX(-${(slideIndex % slides.children.length) * 100}%)`;
+}
